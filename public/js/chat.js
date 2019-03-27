7bf04702ff2e1808a76e2d924e6e1064ba265540
Nathaniel Stephens <ndstephens@gmail.com>
2019-03-27 18:47:37 -0400

    sending location, making it a map link

--- public/js/chat.js
+++ public/js/chat.js
@@ -2,15 +2,32 @@ const socket = io()
 
 const form = document.querySelector('#chat-form')
 const input = document.querySelector('[name=message]')
+const locationBtn = document.querySelector('#send-location')
 
+//? Listen for events emitted on 'message' channel
 socket.on('message', msg => {
   console.log(msg)
 })
 
+//? Form input to send a message to everyone
 form.addEventListener('submit', e => {
   e.preventDefault()
 
   const msg = input.value
   input.value = ''
+  // Send message on 'sendMessage' channel
   socket.emit('sendMessage', msg)
+})
+
+//? Button click to send everyone your location
+locationBtn.addEventListener('click', e => {
+  if (!navigator.geolocation) {
+    return alert('Geolocation is not supported by your browser')
+  }
+
+  navigator.geolocation.getCurrentPosition(position => {
+    const { latitude: lat, longitude: long } = position.coords
+    // Send message on 'sendLocation' channel
+    socket.emit('sendLocation', { lat, long })
+  })
 })
--- src/index.js
+++ src/index.js
@@ -30,11 +30,27 @@ app.get('/', (req, res, next) => {
 //*--------------------------------------------------/
 io.on('connection', socket => {
   console.log('New WebSocket connection')
-
+  // Send message off only to new connection (on the 'message' channel)
   socket.emit('message', 'Welcome!')
+  // Send message to everyone EXCEPT the connection that triggered it
+  socket.broadcast.emit('message', 'A new user has joined')
 
+  // Listen for events emitted on 'sendMessage' channel
   socket.on('sendMessage', msg => {
+    // Send message to everyone, including person who triggered it
     io.emit('message', msg)
+  })
+
+  // Listen for events emitted on 'sendLocation' channel
+  socket.on('sendLocation', loc => {
+    // Send message to everyone, including person who triggered it
+    io.emit('message', `https://google.com/maps?q=${loc.lat},${loc.long}`)
+  })
+
+  // Listen for disconnections events
+  socket.on('disconnect', () => {
+    // Send message to everyone, including person who triggered it
+    io.emit('message', 'A user has left')
   })
 })
 
