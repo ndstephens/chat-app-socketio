f72316033c9279113597575e429072e78cc48ccc
Nathaniel Stephens <ndstephens@gmail.com>
2019-03-28 08:36:36 -0400

    improve UI/UX, add comments

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,25 +1,38 @@
 const socket = io()
 
-const form = document.querySelector('#chat-form')
-const input = document.querySelector('[name=message]')
-const locationBtn = document.querySelector('#send-location')
+//
+//*--------------------------------------------------/
+//*         CACHE UI ELEMENTS
+//*--------------------------------------------------/
+const formEl = document.querySelector('#chat-form')
+const inputEl = document.querySelector('[name=message]')
+const locationBtnEl = document.querySelector('#send-location')
 
-//? Listen for events emitted on 'message' channel
+//
+//*--------------------------------------------------/
+//*         LISTEN FOR MESSAGES -- message
+//*--------------------------------------------------/
 socket.on('message', msg => {
   console.log(msg)
 })
 
 //
 //*--------------------------------------------------/
-//*         MESSAGE FORM
+//*         MESSAGE FORM -- sendMessage
 //*--------------------------------------------------/
-form.addEventListener('submit', e => {
+formEl.addEventListener('submit', e => {
   e.preventDefault()
-  const msg = input.value
-  input.value = ''
+  const msg = inputEl.value.trim()
+
+  if (!msg) {
+    inputEl.focus()
+    return
+  }
 
   // Send message on 'sendMessage' channel
   socket.emit('sendMessage', msg, acknowledgement => {
+    inputEl.value = ''
+    inputEl.focus()
     console.log(acknowledgement)
     //? this optional callback is to be triggered when/if the recipient sends an acknowledgment after receiving the message
   })
@@ -27,14 +40,20 @@ form.addEventListener('submit', e => {
 
 //
 //*--------------------------------------------------/
-//*         LOCATION BUTTON
+//*         LOCATION BUTTON -- sendLocation
 //*--------------------------------------------------/
-locationBtn.addEventListener('click', e => {
+locationBtnEl.addEventListener('click', e => {
   if (!navigator.geolocation) {
     return alert('Geolocation is not supported by your browser')
   }
 
+  locationBtnEl.setAttribute('disabled', 'disabled')
+  locationBtnEl.textContent = 'Fetching location...'
+
   navigator.geolocation.getCurrentPosition(position => {
+    locationBtnEl.removeAttribute('disabled')
+    locationBtnEl.textContent = 'Send Location'
+
     const { latitude: lat, longitude: long } = position.coords
     // Send message on 'sendLocation' channel
     socket.emit('sendLocation', { lat, long }, () => {
